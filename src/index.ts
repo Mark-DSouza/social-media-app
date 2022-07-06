1f30394554fd0328ef5fd6179863b39dcc6ba043
Mark-DSouza <markdsouza434@gmail.com>
2022-07-06 13:31:32 +0530

    Set up Apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { HelloResolver } from "./resolvers/HelloResolver";
 import { DataSource } from "typeorm";
-import {Post} from './entity/Post';
+import { Post } from "./entity/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import "reflect-metadata";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -12,8 +17,18 @@ const AppDataSource = new DataSource({
   logging: true,
   entities: [Post],
 });
+
 const main = async () => {
   await AppDataSource.initialize();
+  const app = express();
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  server.applyMiddleware({ app });
+  app.listen(4000, () => console.log("server started on localhost:4000"));
 };
 
 main().catch((err) => {
